467b42f769df46148b5e39ee2f394db1b3de808b
Leniz <khusaenov@reksoft.ru>
2018-10-12 10:22:32 +0300

    add fun to repository

--- src/main/kotlin/com/example/khusaenov/fluxstarter/repository/ContactRepository.kt
+++ src/main/kotlin/com/example/khusaenov/fluxstarter/repository/ContactRepository.kt
@@ -2,8 +2,12 @@ package com.example.khusaenov.fluxstarter.repository
 
 import com.example.khusaenov.fluxstarter.model.Contact
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository
+import reactor.core.publisher.Flux
 
 /**
  * @author Khusaenov on 10.08.2018
  */
-interface ContactRepository : ReactiveMongoRepository<Contact, String>
+interface ContactRepository : ReactiveMongoRepository<Contact, String> {
+
+    fun getContactByAge(age: Integer): Flux<Contact>
+}
